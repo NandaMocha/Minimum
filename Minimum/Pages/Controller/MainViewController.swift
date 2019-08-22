5d4af9e4a3fae0e409b01a20250de1ea50fb98d7
jessica <chika.workstuff@icloud.com>
2019-08-22 18:55:09 +0700

    1. Adding View Controller and Files Necessary for Application.

---
+++ Minimum/Pages/Controller/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Minimum
+//
+//  Created by Jessica Jacob on 22/08/19.
+//  Copyright © 2019 nandamochammad. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
