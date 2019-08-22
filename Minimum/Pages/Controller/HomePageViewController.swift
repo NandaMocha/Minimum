4cf7c447710284140c5fa4217c6cb5d40d823760
jessica <chika.workstuff@icloud.com>
2019-08-22 19:45:56 +0700

    1.homepage story board 2.button 3.actionsegue to page 2

---
+++ Minimum/Pages/Controller/HomePageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageViewController.swift
+//  Minimum
+//
+//  Created by Jessica Jacob on 22/08/19.
+//  Copyright © 2019 nandamochammad. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
